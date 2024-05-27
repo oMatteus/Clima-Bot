ef684ab7f860141d305b8563ce13ebc31f1fed92
oMatteus <88296251+oMatteus@users.noreply.github.com>
2024-05-26 22:51:16 -0300

    Update bot.cjs

--- bot.cjs
+++ bot.cjs
@@ -84,7 +84,7 @@ app.get('/status', (request, response)=>{
 })
 
 app.get('/get', (request, response)=>{
-    return response.json(clima)
+    return response.json(JSON.stringify(clima))
 })
 
 app.listen(3333)
