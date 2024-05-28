886cb594a17c7f0bf518fd476a7b071ce46e9afc
oMatteus <88296251+oMatteus@users.noreply.github.com>
2024-05-27 23:30:55 -0300

    Update bot.cjs

--- bot.cjs
+++ bot.cjs
@@ -6,11 +6,13 @@ const cors = require('cors')
 console.log('Bot Clima');
 let clima;
 
-async function getClima(){
-    const browser = await puppeteer.launch();
+async function getClima(cidade=guarulhos){
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/chromium-browser'
+      });
     const page = await browser.newPage();
 
-    const cidade = 'Guarulhos';
+    // const cidade = 'Guarulhos';
     const url = `https://www.google.com/search?q=clima+${cidade}&oq=clima+&gs_lcrp=EgZjaHJvbWUqDAgAEAAYQxiABBiKBTIMCAAQABhDGIAEGIoFMhYIARAuGIMBGMcBGLEDGMkDGNEDGIAEMhAIAhAAGIMBGJIDGLEDGIAEMgYIAxBFGDkyDQgEEAAYkgMYgAQYigUyDQgFEAAYgwEYsQMYgAQyCQgGEAAYChiABDIMCAcQABgKGLEDGIAEMg0ICBAAGIMBGLEDGIAEMg0ICRAAGIMBGLEDGIAEqAIAsAIA&sourceid=chrome&ie=UTF-8`;
 
     await page.goto(url);
@@ -34,58 +36,69 @@ async function getClima(){
             }), 
         },
         previsao:[
-            {
-                dia: await page.evaluate(()=>{
-                    return document.querySelector('div[data-wob-di="1"] .Z1VzSb').textContent;
-                }),
-                descricao: await page.evaluate(()=>{
-                    return document.querySelector('div[data-wob-di="1"] .DxhUm img').getAttribute('alt');
-                }),
-                img: await page.evaluate(()=>{
-                    return document.querySelector('div[data-wob-di="1"] .DxhUm img').getAttribute('src');
-                }),
-                tempMax: await page.evaluate(()=>{
-                    return document.querySelector('div[data-wob-di="1"] .gNCp2e .wob_t').textContent;
-                }),
-                tempMin: await page.evaluate(()=>{
-                    return document.querySelector('div[data-wob-di="1"] .QrNVmd .wob_t').textContent;
-                }),
-            },
-            {
-                dia: await page.evaluate(()=>{
-                    return document.querySelector('div[data-wob-di="1"] .Z1VzSb').textContent;
-                }),
-            }
-            
+            // {
+            //     dia: await page.evaluate(()=>{
+            //         return document.querySelector('div[data-wob-di="1"] .Z1VzSb').textContent;
+            //     }),
+            //     descricao: await page.evaluate(()=>{
+            //         return document.querySelector('div[data-wob-di="1"] .DxhUm img').getAttribute('alt');
+            //     }),
+            //     img: await page.evaluate(()=>{
+            //         return document.querySelector('div[data-wob-di="1"] .DxhUm img').getAttribute('src');
+            //     }),
+            //     tempMax: await page.evaluate(()=>{
+            //         return document.querySelector('div[data-wob-di="1"] .gNCp2e .wob_t').textContent;
+            //     }),
+            //     tempMin: await page.evaluate(()=>{
+            //         return document.querySelector('div[data-wob-di="1"] .QrNVmd .wob_t').textContent;
+            //     })
+            // },
         ],
+    };
+
+    for(let i = 0; i<=6; i++){
+        
+        obj = {
+            dia: await page.evaluate((i)=>{
+                return document.querySelector(`div[data-wob-di="${i}"] .Z1VzSb`).textContent;
+            },i),
+            descricao: await page.evaluate((i)=>{
+                return document.querySelector(`div[data-wob-di="${i}"] .DxhUm img`).getAttribute('alt');
+            },i),
+            img: await page.evaluate((i)=>{
+                return document.querySelector(`div[data-wob-di="${i}"] .DxhUm img`).getAttribute('src');
+            },i),
+            tempMax: await page.evaluate((i)=>{
+                return document.querySelector(`div[data-wob-di="${i}"] .gNCp2e .wob_t`).textContent;
+            },i),
+            tempMin: await page.evaluate((i)=>{
+                return document.querySelector(`div[data-wob-di="${i}"] .QrNVmd .wob_t`).textContent;
+            },i)
+        };
+
+        clima.previsao.push(obj)
     };
 
     await browser.close();
     return clima
 };
-
  
-async function start(){
-    console.log('chamou');
-    clima = await getClima();
-    console.log(clima);
-}
-start();
-
-
-
-
+// async function start(){
+//     clima = await getClima();
+//     console.log(clima);
+// }
+// start();
 
 const app = express();
 app.use(cors())
 
-
 app.get('/status', (request, response)=>{
-    return response.send({message:'server is up',status: 200})
+    const hora = new Date;
+    return response.send({message:'server is up',status: 200,data: hora.toLocaleString('pt-BR',{dateStyle: 'full', timeStyle: 'short'})})
 });
 
-app.get('/get', (request, response)=>{
-    console.log('getou');
+app.get('/get:cidade', (request, response)=>{
+    getClima(request.params.cidade);
     return response.json(clima);
 });
 
