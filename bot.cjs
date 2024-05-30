02befb551031e234ae5e2f418b36563e1f499ce7
oMatteus <88296251+oMatteus@users.noreply.github.com>
2024-05-30 13:35:13 -0300

    Update bot.cjs

--- bot.cjs
+++ bot.cjs
@@ -9,65 +9,72 @@ let clima;
 
 
 async function getClima(cidade){
-    const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser'
-      });
-    const page = await browser.newPage();
 
-
-    const url = `https://www.google.com/search?q=clima+${cidade}&oq=clima+&gs_lcrp=EgZjaHJvbWUqDAgAEAAYQxiABBiKBTIMCAAQABhDGIAEGIoFMhYIARAuGIMBGMcBGLEDGMkDGNEDGIAEMhAIAhAAGIMBGJIDGLEDGIAEMgYIAxBFGDkyDQgEEAAYkgMYgAQYigUyDQgFEAAYgwEYsQMYgAQyCQgGEAAYChiABDIMCAcQABgKGLEDGIAEMg0ICBAAGIMBGLEDGIAEMg0ICRAAGIMBGLEDGIAEqAIAsAIA&sourceid=chrome&ie=UTF-8`;
-
-    await page.goto(url);
-
-    const clima = {
-        hoje:{
-            cidade: await page.evaluate(()=>{
-                return document.querySelector('.eKPi4 .BBwThe').textContent;
-            }),
-            dia: await page.evaluate(()=>{
-                return document.querySelector('#wob_dts').textContent;
-            }),
-            temperatura: await page.evaluate(()=>{
-                return document.querySelector('#wob_tm').textContent;
-            }),
-            chuva: await page.evaluate(()=>{
-                return document.querySelector('#wob_pp').textContent;
-            }),
-            descricao: await page.evaluate(()=>{
-                return document.querySelector('#wob_tci').getAttribute('alt');
-            }),
-            img: await page.evaluate(()=>{
-                return document.querySelector('#wob_tci').getAttribute('src');
-            }), 
-        },
-        previsao:[],
-    };
-
-    for(let i = 0; i<=6; i++){
-        
-        obj = {
-            dia: await page.evaluate((i)=>{
-                return document.querySelector(`div[data-wob-di="${i}"] .Z1VzSb`).textContent;
-            },i),
-            descricao: await page.evaluate((i)=>{
-                return document.querySelector(`div[data-wob-di="${i}"] .DxhUm img`).getAttribute('alt');
-            },i),
-            img: await page.evaluate((i)=>{
-                return document.querySelector(`div[data-wob-di="${i}"] .DxhUm img`).getAttribute('src');
-            },i),
-            tempMax: await page.evaluate((i)=>{
-                return document.querySelector(`div[data-wob-di="${i}"] .gNCp2e .wob_t`).textContent;
-            },i),
-            tempMin: await page.evaluate((i)=>{
-                return document.querySelector(`div[data-wob-di="${i}"] .QrNVmd .wob_t`).textContent;
-            },i)
+    try {
+        const browser = await puppeteer.launch({
+            // executablePath: '/usr/bin/chromium-browser'
+          });
+        const page = await browser.newPage();
+    
+    
+        const url = `https://www.google.com/search?q=clima+${cidade}&oq=clima+&gs_lcrp=EgZjaHJvbWUqDAgAEAAYQxiABBiKBTIMCAAQABhDGIAEGIoFMhYIARAuGIMBGMcBGLEDGMkDGNEDGIAEMhAIAhAAGIMBGJIDGLEDGIAEMgYIAxBFGDkyDQgEEAAYkgMYgAQYigUyDQgFEAAYgwEYsQMYgAQyCQgGEAAYChiABDIMCAcQABgKGLEDGIAEMg0ICBAAGIMBGLEDGIAEMg0ICRAAGIMBGLEDGIAEqAIAsAIA&sourceid=chrome&ie=UTF-8`;
+    
+        await page.goto(url);
+    
+        const clima = {
+            hoje:{
+                cidade: await page.evaluate(()=>{
+                    return document.querySelector('.eKPi4 .BBwThe').textContent;
+                }),
+                dia: await page.evaluate(()=>{
+                    return document.querySelector('#wob_dts').textContent;
+                }),
+                temperatura: await page.evaluate(()=>{
+                    return document.querySelector('#wob_tm').textContent;
+                }),
+                chuva: await page.evaluate(()=>{
+                    return document.querySelector('#wob_pp').textContent;
+                }),
+                descricao: await page.evaluate(()=>{
+                    return document.querySelector('#wob_tci').getAttribute('alt');
+                }),
+                img: await page.evaluate(()=>{
+                    return document.querySelector('#wob_tci').getAttribute('src');
+                }), 
+            },
+            previsao:[],
         };
+    
+        for(let i = 0; i<=6; i++){
+            
+            obj = {
+                dia: await page.evaluate((i)=>{
+                    return document.querySelector(`div[data-wob-di="${i}"] .Z1VzSb`).textContent;
+                },i),
+                descricao: await page.evaluate((i)=>{
+                    return document.querySelector(`div[data-wob-di="${i}"] .DxhUm img`).getAttribute('alt');
+                },i),
+                img: await page.evaluate((i)=>{
+                    return document.querySelector(`div[data-wob-di="${i}"] .DxhUm img`).getAttribute('src');
+                },i),
+                tempMax: await page.evaluate((i)=>{
+                    return document.querySelector(`div[data-wob-di="${i}"] .gNCp2e .wob_t`).textContent;
+                },i),
+                tempMin: await page.evaluate((i)=>{
+                    return document.querySelector(`div[data-wob-di="${i}"] .QrNVmd .wob_t`).textContent;
+                },i)
+            };
+    
+            clima.previsao.push(obj)
+        };
+    
+        await browser.close();
 
-        clima.previsao.push(obj)
+        return clima
+    } catch (error) {
+        console.log(error);
+        return error;
     };
-
-    await browser.close();
-    return clima
 };
  
 
@@ -88,6 +95,7 @@ app.get('/status', (request, response)=>{
 });
 
 app.get('/get', (request, response)=>{
-    return response.json(clima);
+    if(clima) return response.json(clima);
+    return response(clima);
 });
 app.listen(3333);
